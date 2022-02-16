3663385efc6f11e357871d4c2c2991cd68a57e69
Arsh Siddiqui <ArshSiddiqui@users.noreply.github.com>
2022-02-15 19:08:49 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+var randNum = Math.floor(Math.random() * 1);
+if (randNum == 0) {
+  document.getElementById("description").innerHTML = "Professional bucatini enjoyer";
+  reload();
+}
+if (randNum == 1) {
+  document.getElementById("description").innerHTML = "Built for leisure, not for speed";
+  reload();
+}
