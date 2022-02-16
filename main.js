899c2a3b6f55e48ef7c364e3d1fad5a85eac8144
Arsh Siddiqui <ArshSiddiqui@users.noreply.github.com>
2022-02-15 19:13:14 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,5 @@
+//To update the description randomly per page reload
 var randNum = Math.floor(Math.random() * 2);
-if (randNum == 0) {
-  document.getElementById("description").innerHTML = "Professional bucatini enjoyer";
-  reload();
-}
-if (randNum == 1) {
-  document.getElementById("description").innerHTML = "Built for leisure, not for speed";
-  reload();
-}
+const descriptions = ["Professional bucatini enjoyer", "Built for leisure, not for speed"];
+document.getElementById("description").innerHTML = descriptions[randNum];
+reload();
