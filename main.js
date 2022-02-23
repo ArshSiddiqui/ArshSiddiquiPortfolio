10b2a3ec63186011dce4056381a623e788780a93
arshsiddiqui <arshsiddiqui@Vt.edu>
2022-02-23 14:32:16 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 //To update the description randomly per page reload
-var randNum = Math.floor(Math.random() * 7);
-const descriptions = ["Professional bucatini enjoyer", "Built for leisure, not for speed", "Person by day, asleep by night", "Was once number one in the world for Phineas and Ferb trivia", "2006 person of the year - Time magazine", "Definitively the opposite of ruthless", "Y-list celebrity"];
+const descriptions = ["My favorite kind of velocity is a non fatal velocity", "Tales from the front of a school bus", "Person by day, asleep by night", "Born to be mild", "Built for leisure, not for speed", "Person by day, asleep by night", "Was once number one in the world for Phineas and Ferb trivia", "2006 person of the year - Time magazine", "Definitively the opposite of ruthless", "Y-list celebrity"];
+var randNum = Math.floor(Math.random() * descriptions.length);
 document.getElementById("description").innerHTML = descriptions[randNum];
 reload();
