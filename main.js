1938a8af5f7cdc067e2393c0643a407074ac4add
arshsiddiqui <arshsiddiqui@gmail.com>
2022-06-17 11:34:38 -0400

    Updates file locations

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-//To update the description randomly per page reload
+// To update the description randomly per page reload
 const descriptions = ["Early to rise, early to bed. Makes a man healthy, but socially dead", "Vaguely European", "My favorite kind of velocity is a non fatal velocity", "Tales from the front of a school bus", "Person by day, asleep by night", "Born to be mild", "Built for leisure, not for speed", "Person by day, asleep by night", "Was once number one in the world for Phineas and Ferb trivia", "2006 person of the year - Time magazine", "Definitively the opposite of ruthless", "Y-list celebrity"];
 var randNum = Math.floor(Math.random() * descriptions.length);
 document.getElementById("description").innerHTML = descriptions[randNum];
-reload();
+
+
+
