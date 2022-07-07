a4545a7c4fd7037fbdd99589f2cf8c48628e3cb3
arshsiddiqui <arshsiddiqui@gmail.com>
2022-07-07 18:31:07 -0400

    New taglines

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 // To update the description randomly per page reload
-const descriptions = ["Early to rise, early to bed. Makes a man healthy, but socially dead", "Vaguely European", "My favorite kind of velocity is a non fatal velocity", "Tales from the front of a school bus", "Person by day, asleep by night", "Born to be mild", "Built for leisure, not for speed", "Person by day, asleep by night", "Was once number one in the world for Phineas and Ferb trivia", "2006 person of the year - Time magazine", "Definitively the opposite of ruthless", "Y-list celebrity"];
+const descriptions = ["Early to rise, early to bed. Makes a man healthy, but socially dead",
+						"Vaguely European", "My favorite kind of velocity is a non fatal velocity", 
+						"Tales from the front of a school bus", "Person by day, asleep by night", 
+						"Born to be mild", "Built for leisure, not for speed", "Person by day, asleep by night",
+						"Was once number one in the world for Phineas and Ferb trivia", "2006 person of the year - Time magazine",
+						"Definitively the opposite of ruthless", "Y-list celebrity", "Professional amateur", "On the cutting edge of walking",
+						"Arsh Siddiqui's primary resource for verifying his job qualifications", "An actual failed writer", 
+						"One of the internet's websites", "Almost the winner of a science fair in 2019"];
 var randNum = Math.floor(Math.random() * descriptions.length);
 document.getElementById("description").innerHTML = descriptions[randNum];
 
